2fbb731dd0da7854b9fdbc31acf6d515c856807a
alejandrocs3719 <alejandro2917asm@gmail.com>
2026-02-22 16:59:17 +0100

    added wallpaper persistence between reboots, added xdg environment variables for scripting

--- modules/home-manager/xdg.nix
+++ modules/home-manager/xdg.nix
@@ -20,10 +20,13 @@
         "libreoffice-writer.desktop";
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "libreoffice-calc.desktop";
       "x-scheme-handler/mailto" = "emacsclient-mail.desktop";
-      
+
       # Default file manager
       "inode/directory" = [ "nemo.desktop" ];
       "application/x-gnome-saved-search" = [ "nemo.desktop" ];
     };
   };
+
+  # Exports XDG_* environment variables, creates XDG dirs automatically, etc.
+  xdg.enable = true;
 }
