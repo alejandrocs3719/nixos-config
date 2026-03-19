00f4feb6db76b418597981d2d87bf9c86e4c3263
alejandrocs3719 <alejandro2917asm@gmail.com>
2026-03-19 14:15:26 +0100

    added kvm config

--- modules/nixos/virtualisation/virtualbox.nix
+++ modules/nixos/virtualisation/virtualbox.nix
@@ -1,15 +1,24 @@
-{ pkgs, lib, config, ... }: {
-  
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+
   options = {
     modules.virtualisation.virtualbox.enable = lib.mkEnableOption "Enables VirtualBox";
   };
 
   config = lib.mkIf config.modules.virtualisation.virtualbox.enable {
-     virtualisation.virtualbox.host = {
-	enable = true;
-	enableKvm = true; # KVM hypervisor
-	addNetworkInterface = false;
-     };
-     users.extraGroups.vboxusers.members = [ "alejandro" ]; # TODO: make username a variable.
+
+    nixpkgs.config.allowUnfree = true;
+    virtualisation.virtualbox.host = {
+      enable = true;
+      enableKvm = true; # KVM hypervisor
+      addNetworkInterface = false;
+      enableExtensionPack = true;
+    };
+    users.extraGroups.vboxusers.members = [ "alejandro" ]; # TODO: make username a variable.
   };
 }
